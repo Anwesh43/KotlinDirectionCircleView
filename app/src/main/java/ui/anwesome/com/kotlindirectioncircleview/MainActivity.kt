ad061246736e3398706b494ce27f831b30a93a17
Anwesh43 <anweshthecool0@gmail.com>
2018-03-13 01:30:26 +0530

    adding DirectionCircleView in Activity class

--- app/src/main/java/ui/anwesome/com/kotlindirectioncircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlindirectioncircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlindirectioncircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.directioncircleview.DirectionCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DirectionCircleView.create(this)
     }
 }
