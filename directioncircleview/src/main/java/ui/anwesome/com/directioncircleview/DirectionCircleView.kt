e8943c0393df56606ac1097c46875ff62d6762f5
Anwesh43 <anweshthecool0@gmail.com>
2018-03-13 00:34:25 +0530

    adding DirectionCircleView class

--- directioncircleview/src/main/java/ui/anwesome/com/directioncircleview/DirectionCircleView.kt
+++ directioncircleview/src/main/java/ui/anwesome/com/directioncircleview/DirectionCircleView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.directioncircleview
 /**
  * Created by anweshmishra on 13/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class DirectionCircleView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
